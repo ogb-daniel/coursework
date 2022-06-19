286a66d9204d72a00999ead6bf2d410a2b8f2d5d
ogb-daniel <ogbuikedaniel@gmail.com>
2022-06-20 01:39:45 +0400

    fetch event

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,26 +1,11 @@
 const staticClub = "school-club-v1"
-// const lessons =  fetch('https://coursewok.herokuapp.com/').then((response)=>
-// response.json()).then((lessons)=>{
-//   return lessons
-// })
+
 const assets = [
   "/",
   "/index.html",
   '/manifest.json',
-  
-//   "/css/style.css",
-//   "/js/app.js",
-//   "/images/coffee1.jpg",
-//   "/images/coffee2.jpg",
-//   "/images/coffee3.jpg",
-//   "/images/coffee4.jpg",
-//   "/images/coffee5.jpg",
-//   "/images/coffee6.jpg",
-//   "/images/coffee7.jpg",
-//   "/images/coffee8.jpg",
-//   "/images/coffee9.jpg",
-]
 
+]
 
 
 self.addEventListener("install", installEvent => {
@@ -33,34 +18,17 @@ self.addEventListener("install", installEvent => {
   )
 })
 self.addEventListener("fetch", fetchEvent => {
+
+
     fetchEvent.respondWith(
-      caches.match(fetchEvent.request).then(res => {
-      return res | fetch(fetchEvent.request).then((response)=>{
-        return caches.open(staticClub).then((cache)=>{
-          cache.put(fetchEvent.request,response.clone())
-          return response
+      caches.open(staticClub).then(function(cache) {
+        return cache.match(fetchEvent.request).then(function(response) {
+          var fetchPromise = fetch(fetchEvent.request).then(function(networkResponse) {
+            cache.put(fetchEvent.request, networkResponse.clone());
+            return networkResponse;
+          })
+          return response || fetchPromise;
         })
       })
-      })
-    )
-
-    // fetchEvent.respondWith((async ()=>{
-    //   const cachedRes = await caches.match(fetchEvent.request);
-    //   if(cachedRes){
-    //     return cachedRes
-    //   } 
-
-    //   const response = await fetch(fetchEvent.request)
-
-    //   // if (!response || response.status !== 200 || response.type !== 'basic') {
-    //   //   return response;
-    //   // }
-    //   if (ENABLE_DYNAMIC_CACHING) {
-    //     const responseToCache = response.clone();
-    //     const cache = await caches.open(staticClub)
-    //     await cache.put(event.request, response.clone());
-    //   }
-    
-    //   return response;
-    // }))
+    );
   })
